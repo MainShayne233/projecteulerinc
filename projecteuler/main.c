c809e5448c1512ee9f981979978b34345917c183
Shayne Tremblay <shaynetremblay@hotmail.com>
2015-07-18 01:19:42 -0400

    Adds to problem 12

--- projecteuler/main.c
+++ projecteuler/main.c
@@ -39,7 +39,7 @@ void problem2(){
 bool isPrime(int num){
     if (num == 2)
         return true;
-    for (int i = 2; i <= (num/2); i+=1){
+    for (int i = 2; i*i<=num; i+=1){
         if (num % i == 0)
             return false;
     }
@@ -67,6 +67,18 @@ int amountOfDigits(int num){
         multiplierIterate *= 10;
         multiplier += multiplierIterate;
     }
+}
+
+
+int * digitsToArray(int number){
+    int length = amountOfDigits(number);
+    int digitsArray[length];
+    for (int i = length; i>0; i-=1){
+        digitsArray[length-i] = number/(pow(10, i-1));
+        number -= digitsArray[length-i]*pow(10, i-1);
+    }
+    int * digitsArrayFinal = digitsArray;
+    return digitsArrayFinal;
 }
 
 
@@ -154,10 +166,12 @@ int problem5(){
             }
         }
         if (circut == 1){
+            printf("%d", number);
             return number;
         }
         number += 1;
     }
+
 }
 
 
@@ -173,7 +187,7 @@ void problem6(){
     
     squareOfSums = pow (squareOfSums, 2);
     
-    printf("%d - %d = %d", squareOfSums, sumOfSquares, (squareOfSums-sumOfSquares));
+    printf("%d", (squareOfSums-sumOfSquares));
     
 }
 
@@ -190,14 +204,78 @@ void problem7(){
 }
 
 void problem8(){
+    static int numList[] = {7, 3, 1, 6, 7, 1, 7, 6, 5, 3, 1, 3, 3, 0, 6, 2, 4, 9, 1, 9, 2, 2, 5, 1, 1, 9, 6, 7, 4, 4, 2, 6, 5, 7, 4, 7, 4, 2, 3, 5, 5, 3, 4, 9, 1, 9, 4, 9, 3, 4, 9, 6, 9, 8, 3, 5, 2, 0, 3, 1, 2, 7, 7, 4, 5, 0, 6, 3, 2, 6, 2, 3, 9, 5, 7, 8, 3, 1, 8, 0, 1, 6, 9, 8, 4, 8, 0, 1, 8, 6, 9, 4, 7, 8, 8, 5, 1, 8, 4, 3, 8, 5, 8, 6, 1, 5, 6, 0, 7, 8, 9, 1, 1, 2, 9, 4, 9, 4, 9, 5, 4, 5, 9, 5, 0, 1, 7, 3, 7, 9, 5, 8, 3, 3, 1, 9, 5, 2, 8, 5, 3, 2, 0, 8, 8, 0, 5, 5, 1, 1, 1, 2, 5, 4, 0, 6, 9, 8, 7, 4, 7, 1, 5, 8, 5, 2, 3, 8, 6, 3, 0, 5, 0, 7, 1, 5, 6, 9, 3, 2, 9, 0, 9, 6, 3, 2, 9, 5, 2, 2, 7, 4, 4, 3, 0, 4, 3, 5, 5, 7, 6, 6, 8, 9, 6, 6, 4, 8, 9, 5, 0, 4, 4, 5, 2, 4, 4, 5, 2, 3, 1, 6, 1, 7, 3, 1, 8, 5, 6, 4, 0, 3, 0, 9, 8, 7, 1, 1, 1, 2, 1, 7, 2, 2, 3, 8, 3, 1, 1, 3, 6, 2, 2, 2, 9, 8, 9, 3, 4, 2, 3, 3, 8, 0, 3, 0, 8, 1, 3, 5, 3, 3, 6, 2, 7, 6, 6, 1, 4, 2, 8, 2, 8, 0, 6, 4, 4, 4, 4, 8, 6, 6, 4, 5, 2, 3, 8, 7, 4, 9, 3, 0, 3, 5, 8, 9, 0, 7, 2, 9, 6, 2, 9, 0, 4, 9, 1, 5, 6, 0, 4, 4, 0, 7, 7, 2, 3, 9, 0, 7, 1, 3, 8, 1, 0, 5, 1, 5, 8, 5, 9, 3, 0, 7, 9, 6, 0, 8, 6, 6, 7, 0, 1, 7, 2, 4, 2, 7, 1, 2, 1, 8, 8, 3, 9, 9, 8, 7, 9, 7, 9, 0, 8, 7, 9, 2, 2, 7, 4, 9, 2, 1, 9, 0, 1, 6, 9, 9, 7, 2, 0, 8, 8, 8, 0, 9, 3, 7, 7, 6, 6, 5, 7, 2, 7, 3, 3, 3, 0, 0, 1, 0, 5, 3, 3, 6, 7, 8, 8, 1, 2, 2, 0, 2, 3, 5, 4, 2, 1, 8, 0, 9, 7, 5, 1, 2, 5, 4, 5, 4, 0, 5, 9, 4, 7, 5, 2, 2, 4, 3, 5, 2, 5, 8, 4, 9, 0, 7, 7, 1, 1, 6, 7, 0, 5, 5, 6, 0, 1, 3, 6, 0, 4, 8, 3, 9, 5, 8, 6, 4, 4, 6, 7, 0, 6, 3, 2, 4, 4, 1, 5, 7, 2, 2, 1, 5, 5, 3, 9, 7, 5, 3, 6, 9, 7, 8, 1, 7, 9, 7, 7, 8, 4, 6, 1, 7, 4, 0, 6, 4, 9, 5, 5, 1, 4, 9, 2, 9, 0, 8, 6, 2, 5, 6, 9, 3, 2, 1, 9, 7, 8, 4, 6, 8, 6, 2, 2, 4, 8, 2, 8, 3, 9, 7, 2, 2, 4, 1, 3, 7, 5, 6, 5, 7, 0, 5, 6, 0, 5, 7, 4, 9, 0, 2, 6, 1, 4, 0, 7, 9, 7, 2, 9, 6, 8, 6, 5, 2, 4, 1, 4, 5, 3, 5, 1, 0, 0, 4, 7, 4, 8, 2, 1, 6, 6, 3, 7, 0, 4, 8, 4, 4, 0, 3, 1, 9, 9, 8, 9, 0, 0, 0, 8, 8, 9, 5, 2, 4, 3, 4, 5, 0, 6, 5, 8, 5, 4, 1, 2, 2, 7, 5, 8, 8, 6, 6, 6, 8, 8, 1, 1, 6, 4, 2, 7, 1, 7, 1, 4, 7, 9, 9, 2, 4, 4, 4, 2, 9, 2, 8, 2, 3, 0, 8, 6, 3, 4, 6, 5, 6, 7, 4, 8, 1, 3, 9, 1, 9, 1, 2, 3, 1, 6, 2, 8, 2, 4, 5, 8, 6, 1, 7, 8, 6, 6, 4, 5, 8, 3, 5, 9, 1, 2, 4, 5, 6, 6, 5, 2, 9, 4, 7, 6, 5, 4, 5, 6, 8, 2, 8, 4, 8, 9, 1, 2, 8, 8, 3, 1, 4, 2, 6, 0, 7, 6, 9, 0, 0, 4, 2, 2, 4, 2, 1, 9, 0, 2, 2, 6, 7, 1, 0, 5, 5, 6, 2, 6, 3, 2, 1, 1, 1, 1, 1, 0, 9, 3, 7, 0, 5, 4, 4, 2, 1, 7, 5, 0, 6, 9, 4, 1, 6, 5, 8, 9, 6, 0, 4, 0, 8, 0, 7, 1, 9, 8, 4, 0, 3, 8, 5, 0, 9, 6, 2, 4, 5, 5, 4, 4, 4, 3, 6, 2, 9, 8, 1, 2, 3, 0, 9, 8, 7, 8, 7, 9, 9, 2, 7, 2, 4, 4, 2, 8, 4, 9, 0, 9, 1, 8, 8, 8, 4, 5, 8, 0, 1, 5, 6, 1, 6, 6, 0, 9, 7, 9, 1, 9, 1, 3, 3, 8, 7, 5, 4, 9, 9, 2, 0, 0, 5, 2, 4, 0, 6, 3, 6, 8, 9, 9, 1, 2, 5, 6, 0, 7, 1, 7, 6, 0, 6, 0, 5, 8, 8, 6, 1, 1, 6, 4, 6, 7, 1, 0, 9, 4, 0, 5, 0, 7, 7, 5, 4, 1, 0, 0, 2, 2, 5, 6, 9, 8, 3, 1, 5, 5, 2, 0, 0, 0, 5, 5, 9, 3, 5, 7, 2, 9, 7, 2, 5, 7, 1, 6, 3, 6, 2, 6, 9, 5, 6, 1, 8, 8, 2, 6, 7, 0, 4, 2, 8, 2, 5, 2, 4, 8, 3, 6, 0, 0, 8, 2, 3, 2, 5, 7, 5, 3, 0, 4, 2, 0, 7, 5, 2, 9, 6, 3, 4, 5, 0};
     
+    int largestProduct = 0;
+    int tempProduct;
+    for (int row = 0; row <= 19; row += 1){
+        for (int start = 0+row*50; start <= 36+row*50; start += 1){
+            tempProduct = 1;
+            for (int i = 0; i <= 13; i += 1){
+                tempProduct *= numList[start+i];
+            }
+            if (tempProduct > largestProduct){
+                largestProduct = tempProduct;
+            }
+        }
+    }
+    printf("%d ", largestProduct);
+}
+
+int problem9(){
+    int solution;
+    for (int a = 1; a < 1000; a+=1){
+        for (int b = 1; b < 1000; b+=1){
+            if ((pow(a,2))+(pow(b,2)) == (pow(1000-a-b, 2))){
+                solution =a*b*(1000-a-b);
+                printf("%d\n", solution);
+                return solution;
+            }
+        }
+    }
+    return 0;
 }
 
 
+long problem10(){
+    int currentPrime = 2;
+    long sum = 0;
+    while (true){
+        sum += currentPrime;
+        currentPrime = nextPrimeNumber(currentPrime);
+        if (currentPrime > 2000000){
+            printf("%ld",sum);
+            return sum;
+        }
+    }
+}
+
+int amountOfDivisors(int number){
+    if (number == 1){
+        return 1;
+    }
+    int divisors = 2;
+    for (int i = 2; i <= pow(number,0.5); i+= 1){
+        if (number % i == 0){
+            divisors += 2;
+        }
+    }
+    return divisors;
+}
+
+int problem11(){
+    int triangleNumber = 0;
+    for (int i = 1; true; i+=1){
+        triangleNumber += i;
+        if (amountOfDivisors(triangleNumber) > 500){
+            printf("%d", triangleNumber);
+            return triangleNumber;
+        }
+    }
+}
 
 
+//DIDN'T DO PROBLEM 11 YET BECAUSE IT'S A GRID
 int main(){
-    problem7();
-    
     
 }
